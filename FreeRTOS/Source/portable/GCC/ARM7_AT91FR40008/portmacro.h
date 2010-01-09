507eaf65d3dea8e876f41bf9a048e442d82dcd0a
FreeRTOS upstream release <dummy@domain.invalid>
2010-01-09 18:14:44 +0100

    import version v6.0.2

--- FreeRTOS/Source/portable/GCC/ARM7_AT91FR40008/portmacro.h
+++ FreeRTOS/Source/portable/GCC/ARM7_AT91FR40008/portmacro.h
@@ -1,5 +1,5 @@
 /*
-    FreeRTOS V6.0.1 - Copyright (C) 2009 Real Time Engineers Ltd.
+    FreeRTOS V6.0.2 - Copyright (C) 2010 Real Time Engineers Ltd.
 
     ***************************************************************************
     *                                                                         *
@@ -113,7 +113,7 @@ extern "C" {
 #define portSTACK_GROWTH			( -1 )
 #define portTICK_RATE_MS			( ( portTickType ) 1000 / configTICK_RATE_HZ )		
 #define portBYTE_ALIGNMENT			8
-#define portYIELD()					asm volatile ( "SWI" )
+#define portYIELD()					asm volatile ( "SWI 0" )
 #define portNOP()					asm volatile ( "NOP" )
 
 /*
--- FreeRTOS/Source/portable/GCC/ARM7_LPC23xx/portmacro.h
+++ FreeRTOS/Source/portable/GCC/ARM7_LPC23xx/portmacro.h
@@ -1,5 +1,5 @@
 /*
-    FreeRTOS V6.0.1 - Copyright (C) 2009 Real Time Engineers Ltd.
+    FreeRTOS V6.0.2 - Copyright (C) 2010 Real Time Engineers Ltd.
 
     ***************************************************************************
     *                                                                         *
@@ -211,7 +211,7 @@ extern volatile unsigned portLONG ulCriticalNesting;					\
 
 
 #define portYIELD_FROM_ISR()		vTaskSwitchContext()
-#define portYIELD()					__asm volatile ( "SWI" )
+#define portYIELD()					__asm volatile ( "SWI 0" )
 /*-----------------------------------------------------------*/
 
 
